787959b03f55bc5138673af64c8d990daf7c1139
tayobonferejo <oluwatayogeorgeige@gmail.com>
2025-07-25 16:54:58 +0100

    more js functionality

--- multiply.js
+++ multiply.js
@@ -11,20 +11,23 @@ window.addEventListener("load", function(){
     // randomNumber()
     const button = document.getElementById("button");
     const inputField = document.getElementById("number");
-    const inputNumber = inputField.value;
-    const ranNum = Math.floor(Math.random() * 6);
+    const ranNum = Number(Math.floor(Math.random() * 6));
+    question.innerText = `${questionGroup[ranNum]}`;
     console.log(ranNum);
+    console.log(`${questionGroup[ranNum]}`);
     let count = 0;
-    question.innerText = `${questionGroup[1]}`;
 
         button.addEventListener("click", function(event){
-            const inputNumber = inputField.value;
+            const inputNumber = Number(inputField.value);
             event.preventDefault();
-            // randomNumber();
+            document.getElementById("myForm").reset();
             
             const answer = [15, 20, 21, 25, 18, 28];
 
-            if(ranNum === 1 && inputNumber === answer[1]){
+            if(ranNum === 0&& inputNumber === answer[0]){
+                count = count + 1;
+            }
+            else if(ranNum === 1 && inputNumber === answer[1]){
                 count = count + 1;
             }
             else if(ranNum === 2 && inputNumber === answer[2]){
@@ -39,21 +42,13 @@ window.addEventListener("load", function(){
             else if(ranNum === 5 && inputNumber === answer[5]){
                 count = count + 1;
             }
-            else if(ranNum === 6 && inputNumber === answer[6]){
-                count = count + 1;
-            }
             else{
                 count = count - 1;
+                console.log(`${ranNum} === 1 && ${inputNumber} === answer[1])`)
             }
-            // console.log(count);
-            // console.log(ranNum);
-            console.log(typeof(inputNumber));
+            console.log(count);
+            const ranNum = Number(Math.floor(Math.random() * 6));
 
         })
-
-        // function randomNumber(){
-        //         const ranNum = Math.floor(Math.random() * 6);
-        //         question.innerText = `${questionGroup[ranNum]}`;
-        // }
 
 });
